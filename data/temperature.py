72e50ae7264b10d9cb03fd795ff034fc03b71266
xilefcul <avivchro@gmail.com>
2022-05-10 16:21:48 +0200

    added content to temperature table

--- data/temperature.py
+++ data/temperature.py
@@ -1,4 +1,4 @@
-from peewee import Model, TextField, CharField, IntegerField, DateTimeField, ForeignKeyField
+from peewee import Model, TextField, DateTimeField, ForeignKeyField, DoubleField
 from machine import Machine
 from datetime import datetime
 from config import DATABASE
@@ -7,8 +7,16 @@ from config import DATABASE
 class Temperature:
     machine = ForeignKeyField(model=Machine, verbose_name="Assoc. Machine", primary_key=True)
 
+    class Meta:
+        database = DATABASE
+
+
 class TemperatureMeasurement:
-    
+    timestamp = DateTimeField(verbose_name="Measurement timestamp", default=datetime.now)
+    current = DoubleField(verbose_name="Current temperature")
+    high = DoubleField(verbose_name="Highest temperature")
+    critical = DoubleField(verbose_name="Critical temperature")
+    label = TextField(verbose_name="Label of the sensor")
 
     class Meta:
         database = DATABASE
