e51b73f7515d454c80d77760937204b446cd2650
xilefcul <avivchro@gmail.com>
2022-05-11 19:23:32 +0200

    changed init.py in data dir

--- data/__init__.py
+++ data/__init__.py
@@ -1,9 +1,6 @@
-# init file is for exporting packages under more convenient way, hold convenience functions
-# allows defining variables at package level
-
-from .cpu import CPU, CPUMeasurement
-from .energy import EnergyMonitor, EnergyMeasurement
-from .machine import Machine
-from .memory import Memory, MemoryMeasurement
-from .gpu import GPU, GPUMeasurement
-from .temperature import Temperature, TemperatureMeasurement
+from data.cpu import CPU, CPUMeasurement
+from data.energy import EnergyMonitor, EnergyMeasurement
+from data.machine import Machine
+from data.memory import Memory, MemoryMeasurement
+from data.gpu import GPU, GPUMeasurement
+from data.temperature import Temperature, TemperatureMeasurement
