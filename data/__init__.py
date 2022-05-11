f2059a30aa14e7271b2f873671b3f453af90504a
xilefcul <avivchro@gmail.com>
2022-05-11 21:51:01 +0200

    changed import statements

--- data/__init__.py
+++ data/__init__.py
@@ -1,6 +1,6 @@
-from data.cpu import CPU, CPUMeasurement
-from data.energy import EnergyMonitor, EnergyMeasurement
-from data.machine import Machine
-from data.memory import Memory, MemoryMeasurement
-from data.gpu import GPU, GPUMeasurement
-from data.temperature import Temperature, TemperatureMeasurement
+from .cpu import CPU, CPUMeasurement
+from .energy import EnergyMonitor, EnergyMeasurement
+from .machine import Machine
+from .memory import Memory, MemoryMeasurement
+from .gpu import GPU, GPUMeasurement
+from .temperature import Temperature, TemperatureMeasurement
